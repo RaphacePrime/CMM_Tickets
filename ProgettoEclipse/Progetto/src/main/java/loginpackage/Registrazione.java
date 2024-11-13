0cc4acdc90969b00560cc9407853109ca3e8133f
MicheleCaprinali <m.caprinali@studenti.unibg.it>
2024-11-13 08:57:47 +0100

    fix errors registrazione.java

--- ProgettoEclipse/Progetto/src/main/java/loginpackage/Registrazione.java
+++ ProgettoEclipse/Progetto/src/main/java/loginpackage/Registrazione.java
@@ -8,13 +8,15 @@ import databasepackage.Database;
 
 public class Registrazione {
     public static boolean registraUtente(Utente utente) {
-        String sql = "INSERT INTO utenti (username, password, codice_fiscale, admin) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO utenti (username, password, codice_fiscale, telefono, email, admin) VALUES (?, ?, ?, ?, ?, ?)";
 
         try (Connection conn = Database.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, utente.getUsername());
             pstmt.setString(2, utente.getPassword());
             pstmt.setString(3, utente.getCodiceFiscale());
-            pstmt.setBoolean(4, utente.isAdmin());
+            pstmt.setString(4, utente.getTelefono());
+            pstmt.setString(5, utente.getEmail());
+            pstmt.setBoolean(6, utente.isAdmin());
             pstmt.executeUpdate();
             System.out.println("Registrazione avvenuta con successo!");
             return true;
